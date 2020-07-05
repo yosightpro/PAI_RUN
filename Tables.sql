e515703f4607d12caffc692ebe78d66bc1884756
yosightpro <gates22.bn@gmail.com>
2020-07-05 13:46:55 +0300

    Create other statistc sheets and table.sql

---
+++ Tables.sql
@@ -0,0 +1,77 @@
+CREATE TABLE ibrd_ug.borrower
+(
+   borrower_id           integer   DEFAULT nextval('ibrd_ug.borrowers_borrower_id_seq'::regclass) NOT NULL,
+   borrower              text,
+   borrowers_obligation  text
+);
+
+CREATE TABLE ibrd_ug.country
+(
+   country_id    integer   DEFAULT nextval('ibrd_ug.countries_country_id_seq'::regclass) NOT NULL,
+   country_name  text,
+   fk_region_id  integer   NOT NULL,
+   country_code  text
+);
+
+CREATE TABLE ibrd_ug.data_loading_log
+(
+   log_id             serial      NOT NULL,
+   file_name          text,
+   time_started       timestamp,
+   time_finished      timestamp,
+   records_processed  text
+);
+
+CREATE TABLE ibrd_ug.guarantor
+(
+   guarantor_id            integer   DEFAULT nextval('ibrd_ug.guarantors_guarantor_id_seq'::regclass) NOT NULL,
+   guarantor               text,
+   guarantor_country_code  text
+);
+
+CREATE TABLE ibrd_ug.loan
+(
+   loan_id          integer   DEFAULT nextval('ibrd_ug.loans_loan_id_seq'::regclass) NOT NULL,
+   loan_number      text,
+   loan_type        text,
+   loan_status      text,
+   fk_region_id     integer,
+   fk_country_id    integer,
+   fk_guarantor_id  integer,
+   fk_borrower_id   integer
+);
+
+CREATE TABLE ibrd_ug.loan_details
+(
+   loan_details_id             serial    NOT NULL,
+   fk_loan_id                  integer,
+   end_of_period               text,
+   interest_rate               text,
+   currencyof_commitment       text,
+   project_id                  text,
+   project_name                text,
+   original_principal_amount   numeric,
+   cancelled_amount            numeric,
+   undisbursed_amount          numeric,
+   disbursed_amount            numeric,
+   repaid_to_ibrd              numeric,
+   due_to_ibrd                 numeric,
+   exchange_adjustment         numeric,
+   sold_3rd_party              numeric,
+   repaid_3rd_party            numeric,
+   due_3rd_party               numeric,
+   first_repayment_date        text,
+   last_repayment_date         text,
+   agreement_signing_date      text,
+   board_approval_date         text,
+   effective_date_most_recent  text,
+   closed_date_most_recent     text,
+   last_disbursement_date      text,
+   loans_held                  numeric
+);
+
+CREATE TABLE ibrd_ug.region
+(
+   region_id    integer   DEFAULT nextval('ibrd_ug.regions_region_id_seq'::regclass) NOT NULL,
+   region_name  text      NOT NULL
+);
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import csv
 import psycopg2
 from xlwt import Workbook
+from datetime import datetime
 
 
 try:
@@ -12,9 +13,180 @@ except psycopg2.DatabaseError as err:
     print('Error %s' % err)
 
 # data supplier is PI would name file with date and name as (YYYYMMDDD_ibrd.csv)
+csv_filename = 'ibrd.csv'
+cur.execute(
+    "insert into data_loading_log(file_name, time_started) values ('%s', now())" % csv_filename)
+conn.commit()
+# get most recent log id
+cur.execute("select max(log_id) from data_loading_log")
+result = cur.fetchone()
+log_id = result[0]
+reader = csv.reader(open(csv_filename, 'r'), delimiter=',')
+num_of_rows = 0
+for index, row in enumerate(reader):
+    if index == 0:
+        continue
 
+    region_name = row[2].replace("'", "")
+    country_code = row[3]
+    country_name = row[4].replace("'", "")
+    borrower = row[5].replace("'", "")
+    guarantor_country_code = row[6]
+    guarantor_name = row[7].replace("'", "")
+    loan_number = row[1]
+    loan_type = row[8]
+    loan_status = row[9]
+    end_of_period = row[0]
+    interest_rate = row[10]
+    currencyof_commitment = row[11]
+    project_id = row[12]
+    project_name = row[13].replace("'", "")
+    original_principal_amount = row[14]
+    cancelled_amount = row[15]
+    undisbursed_amount = row[16]
+    disbursed_amount = row[17]
+    repaid_to_ibrd = row[18]
+    due_to_ibrd = row[19]
+    exchange_adjustment = row[20]
+    borrowers_obligation = [21]
+    sold_3rd_party = row[22]
+    repaid_3rd_party = row[23]
+    due_3rd_party = row[24]
+    loans_held = row[25]
+    first_repayment_date = row[26]
+    last_repayment_date = row[27]
+    agreement_signing_date = row[28]
+    board_approval_date = row[29]
+    effective_date_most_recent = row[30]
+    closed_date_most_recent = row[31]
+    last_disbursement_date = row[32]
+    # Region
+# REGION
+    try:
+        get_region = "Select region_id from region where region_name ilike '%s'" % region_name
+        cur.execute(get_region)
+        result = cur.fetchone()
 
-# Average for original_principal_amount
+        if result:
+            region_id = result[0]
+        else:  # region doesnt exist yet, create it
+            insert_region = "INSERT INTO region (region_name) VALUES ('%s')" % region_name
+            cur.execute(insert_region)
+            conn.commit()
+
+            # fetch after insert
+            cur.execute(get_region)
+            result = cur.fetchone()
+            region_id = result[0]
+            print("Created Region ID: ", region_id)
+
+    except Exception as err:
+        print("Region Error Occurred: ", err)
+
+    # COUNTRY
+    try:
+        get_ctry = "select country_id from country where country_name ilike '%s'" % country_name
+        cur.execute(get_ctry)
+        result = cur.fetchone()
+
+        if result:
+            ctry_id = result[0]
+        else:  # country doesnt exist yet, create it
+            insert_country = "INSERT INTO country (country_name, country_code, fk_region_id) VALUES ('%s', '%s', %d)" % (
+                country_name, country_code, region_id)
+            cur.execute(insert_country)
+            conn.commit()
+
+            # fetch after insert
+            cur.execute(get_ctry)
+            result = cur.fetchone()
+            ctry_id = result[0]
+            print("Created Country ID: ", ctry_id)
+
+    except Exception as err:
+        print("Country Error Occurred: ", err)
+
+    # BORROWER
+    try:
+        get_borrower = "select borrower_id from borrower where borrower ilike '%s'" % borrower
+        cur.execute(get_borrower)
+        result = cur.fetchone()
+
+        if result:
+            borrower_id = result[0]
+        else:  # borrower doesnt exist yet, create it
+            insert_borrower = "INSERT INTO borrower (borrower, borrowers_obligation) VALUES ('%s', '%s')" % (
+                borrower, borrowers_obligation)
+            cur.execute(insert_borrower)
+            conn.commit()
+
+            # fetch after insert
+            cur.execute(get_borrower)
+            result = cur.fetchone()
+            borrower_id = result[0]
+            print("Created Borrower ID: ", borrower_id)
+
+    except Exception as err:
+        print("Borrower Error Occurred: ", err)
+
+    # GUARANTOR
+    try:
+        get_guarantor = "select guarantor_id from guarantor where guarantor ilike '%s'" % guarantor_name
+        cur.execute(get_guarantor)
+        result = cur.fetchone()
+
+        if result:
+            guarantor_id = result[0]
+        else:  # guarantor doesnt exist yet, create it
+            insert_country = "INSERT INTO guarantor (guarantor, guarantor_country_code) VALUES ('%s', '%s')" % (
+                guarantor_name, guarantor_country_code)
+            guarantor_id = cur.execute(insert_country)
+            conn.commit()
+
+            # fetch after insert
+            cur.execute(get_guarantor)
+            result = cur.fetchone()
+            guarantor_id = result[0]
+            print("Created Guarantor ID: ", guarantor_id)
+
+    except Exception as err:
+        print("Guarantor Error Occurred: ", err)
+   # LOAN
+    try:
+        insert_loan = """INSERT INTO loan (loan_number, loan_status, loan_type, 
+            fk_borrower_id, fk_region_id, fk_guarantor_id, fk_country_id) VALUES 
+            ('%s', '%s', '%s', %d, %d, %d, %d)""" % (loan_number, loan_status, loan_type, borrower_id, region_id, guarantor_id, ctry_id)
+        cur.execute(insert_loan)
+        conn.commit()
+        # fetch most recent loan entry just created. Since there are monthly insertions with same loan number
+        get_loan = """select max(loan_id) from loan where loan_number ilike '%s'""" % loan_number
+        cur.execute(get_loan)
+        result = cur.fetchone()
+        loan_id = result[0]
+
+    except Exception as err:
+        print("Loan Error Occurred: ", err)
+
+        # LOAN DETAILS
+    try:
+        insert_details = """insert into loan_details (fk_loan_id,end_of_period,interest_rate,currencyof_commitment,project_id,project_name,original_principal_amount,cancelled_amount,undisbursed_amount,disbursed_amount,repaid_to_ibrd,due_to_ibrd,exchange_adjustment,sold_3rd_party,repaid_3rd_party,due_3rd_party,first_repayment_date,last_repayment_date,agreement_signing_date,board_approval_date,effective_date_most_recent,closed_date_most_recent,last_disbursement_date,loans_held) VALUES (%d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')""" % (
+            loan_id, end_of_period, interest_rate, currencyof_commitment, project_id, project_name, original_principal_amount, cancelled_amount, undisbursed_amount, disbursed_amount, repaid_to_ibrd, due_to_ibrd, exchange_adjustment, sold_3rd_party, repaid_3rd_party, due_3rd_party, first_repayment_date, last_repayment_date, agreement_signing_date, board_approval_date, effective_date_most_recent, closed_date_most_recent, last_disbursement_date, loans_held)
+        cur.execute(insert_details)
+        conn.commit()
+
+    except:
+        print("Loan Details Error Occurred: ", err)
+
+    num_of_rows += 1
+    print(num_of_rows)
+# if index > 7:
+#   exit()
+# update after process ends
+cur.execute(
+    "update data_loading_log set time_finished = now(), records_processed = %d where log_id = %d" % (num_of_rows, log_id))
+conn.commit()
+
+# Excel Workbook build set up
 
 main_workbook = Workbook()
 
@@ -33,6 +205,26 @@ def generate_excel(headers_one, batch_one, main_workbook, sheet_name):
             sheet.write(row_index, col_index, field)
         row_index += 1
 
+# Process Summary
+
+
+def get_process_summary():
+    process_summary = "Select file_name, time_started, time_finished, records_processed from ibrd_ug.data_loading_log where log_id = 21"  # % log_id
+
+    cur.execute(process_summary)
+    avg_rows = cur.fetchall()
+
+    return avg_rows
+
+
+rowsprocessed = get_process_summary()
+avg_header = ["File Name", "Time Started",
+              "Fime_Finished", "Records Rrocessed"]
+generate_excel(avg_header, rowsprocessed, main_workbook, "Process Summary")
+main_workbook.save("Summary.xls")
+
+
+# # Average for original_principal_amount
 
 def get_averages_per_country():
     average_principal_per_ctry = """select TEMP.country_name, avg(TEMP.original_principal_amount) from 
@@ -47,8 +239,8 @@ def get_averages_per_country():
 
 
 rows = get_averages_per_country()
-avg_header = ["Country", "Average Principal"]
-generate_excel(avg_header, rows, main_workbook, "Averages_Per_Country")
+avg_header = ["Country", "Average Original Principal Amount"]
+generate_excel(avg_header, rows, main_workbook, "Averages_Principal_Amount")
 
 # Average for cancelled_amount
 
@@ -67,7 +259,7 @@ def get_averages_cancelled_amount_per_country():
 
 rows = get_averages_cancelled_amount_per_country()
 avg_header = ["Country", "Average Cancelled"]
-generate_excel(avg_header, rows, main_workbook, "Cancelled_Per_Country")
+generate_excel(avg_header, rows, main_workbook, "Cancelled_Amount_Per_Country")
 
 main_workbook.save("Summary.xls")
 
